53d1651283adc0a4aa72b08bf5b2d9bbdee3a91e
jayyski <88512354+jayyski@users.noreply.github.com>
2023-07-21 13:52:49 -0400

    Add files via upload
    Completed

--- main.py
+++ main.py
@@ -1,24 +1,64 @@
 import requests
 from pprint import pprint
+from colorama import init, Back, Style
 
-def abilities(ability, pokemon_name):
+def abilities(ability,pokemon_name):
     try:
         response = requests.get(f'https://pokeapi.co/api/v2/move/{ability}/').json()
     except:
-        return print('Error: Ability or pokemon not found')
+        return print(f'{Back.RED}Ability or pokemon not found')
 
-    type = response['type']['name']
+    learned_by_pokemon = response['learned_by_pokemon']
+    power = response['power']
+    priority = response['priority']
     accuracy = response['accuracy']
-    effect = response['effect_entries'][0]['effect']
-    learned_pokemon = response['learned_by_pokemon']
     
-    for pokemon in learned_pokemon:
-        if pokemon_name == pokemon['name']:
-            return print(f'\n{pokemon_name} can use {ability} with {accuracy} accuracy. \n{pokemon_name} is a {type} type pokemon. \n{effect}')   
-        return print(f'\n{pokemon_name} cannot use {ability}')
+    for pokemon in learned_by_pokemon:
+        if pokemon_name in pokemon['name']:
+             print(f'\n{Back.BLUE}{pokemon_name.capitalize()} can learn {ability.capitalize()}')
+             print(f'{Back.BLUE}Power: {power}')
+             print(f'{Back.BLUE}Priority: {priority}')
+             print(f'{Back.BLUE}Accuracy: {accuracy}')
+             return
+            
+    return print(f'{Back.RED}{pokemon_name.capitalize()} can\'t learn {ability.capitalize()}')
 
+    
+def stats_abilities(pokemon_name):
+    try:
+        response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}/').json()
+    except:
+        return print(f'{Back.RED}Error: Pokemon not found')
+    
+    moves = response['moves']
+    stats = response['stats']
+
+    print(f'{Back.BLUE}' + pokemon_name.capitalize(), end='\n\n')
+    print(f'{Back.BLUE}Abilities', end='\n\n')
+    
+    for move in moves:
+        print (f'{Back.BLUE}-' + move['move']['name']) 
+    
+    print(f'\nBase stats', end='\n\n')
+    
+    for stat in stats:
+        print(f'{Back.BLUE}' + stat['stat']['name'] + ': ' + str(stat['base_stat']))
+    
 while True:
-    input_ability = input('\nEnter ability: ').replace(' ', '-')
-    pokemon = input('Enter pokemon: ').replace(' ', '-')
-    abilities(input_ability, pokemon)
+    init(autoreset=True)
+    print('--------------------------')
+    choice = input('1. Check pokemon abilities \n2. Check pokemon stats \n3. Exit \n\nEnter option: ')
+    
+    if choice == '1':
+        input_ability = input('\nEnter ability: ').replace(' ', '-')
+        pokemon = input('\nEnter pokemon: ').replace(' ', '-')
+        abilities(input_ability,pokemon)
+    elif choice == '2':
+        pokemon = input('\nEnter ability: ').replace(' ', '-')
+        stats_abilities(pokemon)
+    elif choice == '3':
+        break
+    else:
+        print(f'{Back.RED}\nInvalid option')
+    
 
